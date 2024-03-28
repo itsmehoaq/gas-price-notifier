84cfb8e7bcd0fa7eee74e9b057d09600544a94f8
Mune <mune@mune.moe>
2024-03-28 21:06:12 +0700

    fix: fix diff prices not calculated correctly

--- deploy-commands.js
+++ deploy-commands.js
@@ -13,10 +13,12 @@ for (const folder of commandFolders) {
     // Grab all the command files from the commands directory you created earlier
     const commandsPath = path.join(foldersPath, folder);
     const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
     // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
     for (const file of commandFiles) {
         const filePath = path.join(commandsPath, file);
         const command = require(filePath);
+
         if ('data' in command && 'execute' in command) {
             commands.push(command.data.toJSON());
         } else {
